f4d2730bd15c090bea0e3b66a0e7697dc58df5d5
sadaramdeepthi <sadaramdeepthi@gmail.com>
2020-12-14 17:36:07 +0530

    squashing all commits

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Table from "./components/Table/Table";
+import "./App.scss";
+
+const App = () => {
+  return (
+    <div className="App">
+      <h2>User Details</h2>
+
+      <Table />
+    </div>
+  );
+};
+
+export default App;
