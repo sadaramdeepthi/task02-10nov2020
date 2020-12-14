f00858edde24830e59f7d932466c7ae2a05c0e70
sadaramdeepthi <sadaramdeepthi@gmail.com>
2020-12-14 21:36:53 +0530

    modification as per the feedback

--- src/components/Table/Table.jsx
+++ src/components/Table/Table.jsx
@@ -6,6 +6,13 @@ import Pagination from "./Pagination";
 
 const Table = () => {
   const [users, setUsers] = useState([]);
+  const [jsonColumn] = useState([
+    { col: "id", width: "150" },
+    { col: "name", width: "100" },
+    { col: "email", width: "150" },
+    { col: "gender", width: "100" },
+    { col: "status", width: "150" },
+  ]);
   const [loading, setLoading] = useState(true);
   const [activeIndex, setActiveIndex] = useState(1);
   const [currentlySortedField, setCurrentlySortedField] = useState("id");
@@ -17,14 +24,19 @@ const Table = () => {
   const [usersPerPage, setUsersPerPage] = useState(10);
 
   // Fetching data from API
-  useEffect(async () => {
-    const response = await fetch("https://gorest.co.in/public-api/users");
-    const data = await response.json();
-    console.log(data);
-    console.log(data.data);
-    setUsers(data.data);
-    setLoading(false);
-  }, []);
+  useEffect(() => {
+    const fetchingUserData = async () => {
+      try {
+        const response = await fetch("https://gorest.co.in/public-api/users");
+        const data = await response.json();
+        setUsers(data.data);
+        setLoading(false);
+      } catch (err) {
+        setLoading(true);
+      }
+    };
+    fetchingUserData();
+  }, [setUsers, setLoading]);
 
   const handleRowClick = (item, rowIndex) => {
     setActiveIndex(rowIndex);
@@ -55,7 +67,6 @@ const Table = () => {
 
   // Changing the page
   const paginate = (pageNumber) => {
-    console.log(pageNumber);
     setCurrentPage(pageNumber + 1);
   };
 
@@ -75,57 +86,20 @@ const Table = () => {
           onRowClick={handleRowClick}
           onSort={handleSorting}
         >
-          <DataTable.Column
-            field="id"
-            align="center"
-            width={100}
-            hasBorderLeft
-            isSortable
-          >
-            ID No.
-          </DataTable.Column>
-
-          <DataTable.Column
-            field="name"
-            align="left"
-            hasBorderLeft
-            hasBorderRight
-            isSortable
-            isResizable
-          >
-            Name
-          </DataTable.Column>
-
-          <DataTable.Column
-            field="email"
-            align="left"
-            hasBorderLeft
-            hasBorderRight
-            isSortable
-          >
-            Email
-          </DataTable.Column>
-
-          <DataTable.Column
-            field="gender"
-            align="left"
-            width={150}
-            hasBorderLeft
-            hasBorderRight
-            isSortable
-          >
-            Gender
-          </DataTable.Column>
-
-          <DataTable.Column
-            field="status"
-            align="left"
-            width={100}
-            hasBorderLeft
-            hasBorderRight
-          >
-            Status
-          </DataTable.Column>
+          {_.map(jsonColumn, (row) => {
+            return (
+              <DataTable.Column
+                field={`${row.col}`}
+                align="left"
+                width={`${row.width}`}
+                align="left"
+                hasBorderLeft
+                isSortable
+              >
+                {_.upperCase(row.col)}
+              </DataTable.Column>
+            );
+          })}
         </DataTable>
       )}
 
--- src/components/Table/Table.spec.jsx
+++ src/components/Table/Table.spec.jsx
@@ -11,4 +11,14 @@ describe("Table", () => {
     const table = component.find(".datatable-wrapper");
     expect(table.exists()).toBe(true);
   });
+
+  it("checking if users data is not loaded, then loading message displays", () => {
+    const component = mount(<Table />);
+    const table = component.find("DataTable");
+    expect(table.exists()).toBe(false);
+    const noDataWrapper = component.find(".noData-wrapper");
+    expect(noDataWrapper.text()).toBe(
+      "Please Hold on, fetching data may take some time..."
+    );
+  });
 });
